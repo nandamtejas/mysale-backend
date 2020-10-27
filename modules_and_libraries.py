ea6cb958992dbdd0780e64962cdb193a460c9829
Tejas Nandam <tejasnandam66@gmail.com>
2020-10-27 15:47:18 +0530

    ads

--- modules_and_libraries.py
+++ modules_and_libraries.py
@@ -1,19 +1,22 @@
-from flask import Flask, Response, request, render_template
-from flask_restx import Api, abort, fields, Resource
-from flask_restx.reqparse import RequestParser
-from flask_restx.inputs import positive, URL
-from flask_jwt_extended import JWTManager, jwt_required, decode_token, create_access_token, get_jwt_identity
-from flask_bcrypt import generate_password_hash, check_password_hash, Bcrypt
-from flask_sqlalchemy import SQLAlchemy
-from jwt.exceptions import *
-from flask_jwt_extended.exceptions import *
-from werkzeug.exceptions import BadRequest
-import datetime as dt
-from datetime import datetime, timedelta
-import pytz
-import config
-from flask_cors import CORS
-from sqlalchemy.exc import *
-from sqlalchemy.orm.exc import *
-from flask_mail import Mail
-from errors import *
+try:
+    from flask import Flask, Response, request, render_template
+    from flask_restx import Api, abort, fields, Resource
+    from flask_restx.reqparse import RequestParser
+    from flask_restx.inputs import positive, URL
+    from flask_jwt_extended import JWTManager, jwt_required, decode_token, create_access_token, get_jwt_identity
+    from flask_bcrypt import generate_password_hash, check_password_hash, Bcrypt
+    from flask_sqlalchemy import SQLAlchemy
+    from jwt.exceptions import *
+    from flask_jwt_extended.exceptions import *
+    from werkzeug.exceptions import BadRequest
+    import datetime as dt
+    from datetime import datetime, timedelta
+    import pytz
+    import config
+    from flask_cors import CORS
+    from sqlalchemy.exc import *
+    from sqlalchemy.orm.exc import *
+    from flask_mail import Mail
+    from errors import *
+except Exception as e:
+    return {'message': str(e)}, 400
